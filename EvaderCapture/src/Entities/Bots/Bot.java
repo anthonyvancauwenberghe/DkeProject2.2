77280804f29c5fd16b493b7926ea5f208285bbb2
Krulvis <joepkleint@gmail.com>
2017-06-18 13:13:58 +0200

    Laying base for entities

--- EvaderCapture/src/Entities/Bots/Bot.java
+++ EvaderCapture/src/Entities/Bots/Bot.java
@@ -1,7 +1,9 @@
 package Entities.Bots;
 
+import Entities.Entity;
+
 /**
  * Created by Tony on 17/06/2017.
  */
-public class Bot {
+public class Bot extends Entity {
 }
--- EvaderCapture/src/Entities/Players/Player.java
+++ EvaderCapture/src/Entities/Players/Player.java
@@ -1,7 +1,9 @@
 package Entities.Players;
 
+import Entities.Entity;
+
 /**
  * Created by Tony on 17/06/2017.
  */
-public class Player {
+public class Player extends Entity {
 }
